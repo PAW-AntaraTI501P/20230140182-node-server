5764430a92e02b9732f9b7f0e43f2e8a5a1447c3
aivylv <lyvialarasati@gmail.com>
2025-08-05 09:03:36 +0700

    pertemuan 3

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const port = 3001;
-app.use(cors());
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from Node.js Server!" });
+const port = 5000;
+
+app.use(express.json());
+
+app.set("view engine", "ejs"); //utk ke halaman ejs
+
+app.get('/', (req, res) => {
+    res.render("index"); //render file ke index.ejs
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact"); //render ke file contact.ejs
+})
+
+app.use((req, res) => {
+    res.status(404).send("404 - Page not found");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+    console.log(`Server running on http://localhost:${port}`);
+})
